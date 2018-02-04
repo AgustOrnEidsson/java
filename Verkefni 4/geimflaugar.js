303104ebf2f5d3bd4357a750f1116786205674af
Kalliarolunni <agustei295@nemi.tskoli.is>
2018-02-04 23:56:16 +0000

    búinn

--- Verkefni 4/geimflaugar.js
+++ Verkefni 4/geimflaugar.js
@@ -11,12 +11,22 @@ var geimflaug2 = new geimflaugar;
 var geimflaug3 = new geimflaugar;
 
 geimflaug1.name="Geimflaug 1";
-console.log(geimflaug1)
+geimflaug1.showName()
 
-for (var geimflaug in geimflaugar) {
-  geimflaugar[geimflaug].speed;
+listi=[geimflaug1,geimflaug2,geimflaug3]
+
+for (var geimflaug in listi) {
+  listi[geimflaug].speed=5
 }
 
 geimflaugar.prototype.fly=function(){
 		this.speed++
 	};
+
+var geimflaug4 = new geimflaugar;
+
+geimflaug4.speed=5
+
+geimflaug4.damage=function(){
+	this.life--
+	}
