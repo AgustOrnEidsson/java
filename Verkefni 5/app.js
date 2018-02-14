1dce78e46d5277c585ba28ed89fbbece7feaa9b3
Kalliarolunni <agustei295@nemi.tskoli.is>
2018-02-14 14:02:15 +0000

    fann typo

--- Verkefni 5/app.js
+++ Verkefni 5/app.js
@@ -5,6 +5,7 @@ function quiz(spurn,svar,annad){
 };
 var spurn1 = new quiz("Hvaða drykkur kemur beint úr beljum?","Mjólk",["Kaffi","Kristall Plús","Mjólk","Ananas safi"]);
 var spurn2 = new quiz("Fer ananas á pizzu?","True",["False","True"]);
+var spurn3= new quiz("Er pylsa samloka?","True",["True","False"])
 
 list=[spurn1,spurn2]
 
@@ -13,13 +14,13 @@ tala=Math.round(Math.random());
 spur=list[tala]
 
 function uppl(spurnin,svarid,anna){
-	document.getElementById("quiz").innerHTML +="<div>"+spurn1.spurn+"</div>"+"<br>";
+	document.getElementById("quiz").innerHTML +='<div style="color:blue;">'+spurnin+"</div>"+"<br>";
 	for (x in anna) {
 		if (anna[x]==svarid) {
-    		document.getElementById("quiz").innerHTML += '<div style="color:red;">'+ anna[x]+"</div>";
+    		document.getElementById("quiz").innerHTML += '<div style="color:lime;">'+ anna[x]+"</div>";
 		}
 		else{
-			document.getElementById("quiz").innerHTML += "<div>"+ anna[x]+"</div>";
+			document.getElementById("quiz").innerHTML += '<div style="color:red;">'+ anna[x]+"</div>";
 		}
 	}
 };
